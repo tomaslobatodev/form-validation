958097e7a48b51a4cd922dff934cd3f73341e5a6
Tomas Lobato <tomaslobatodev@gmail.com>
2023-09-01 15:54:17 -0300

    password validation and confirmation

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const fullname = document.querySelector("#name")
 const email = document.querySelector("#email");
 const country = document.querySelector("#country")
 const zipcode = document.querySelector("#zipcode")
+const password = document.querySelector("#password")
+const confirmPassword = document.querySelector("#confirmpassword")
+const msg = document.querySelector("#message")
 
 const submit = document.querySelector("#submitbtn")
 
 //when the user leaves an input empty
 function fieldRequired(errormsg, input) {
-  errormsg.textContent = "This field is required buddy"
+  errormsg.textContent = "This field is required"
   input.classList.add("invalid");
 }
 
@@ -16,7 +19,9 @@ function invalidInput(errormsg, input) {
   if (input === fullname) errormsg.textContent = "Enter your real full name please"
   else if (input === email) errormsg.textContent = "I'm expecting an email adress"
   else if (input === country) errormsg.textContent = "I'm sure that's not your country"
-  else if (input === zipcode) errormsg.textContent = "That's not your zip code man"
+  else if (input === zipcode) errormsg.textContent = "That's not your zip code"
+  else if (input === password) errormsg.textContent = "Consider using a stronger password"
+  else if (input === confirmPassword) errormsg.textContent = "Consider using a stronger password"
   input.classList.add("invalid")
 }
 
@@ -34,14 +39,30 @@ submit.addEventListener("click", (ev) => {
   const emailError = document.querySelector("#email-error")
   const countryError = document.querySelector("#country-error")
   const zipcodeError = document.querySelector("#zipcode-error")
+  const passwordError = document.querySelector("#password-error")
+  const confirmPasswordError = document.querySelector("#confirmpassword-error")
   
   if (fullname.validity.valueMissing) fieldRequired(nameError, fullname)
   
-  if (email.validity.valueMissing) fieldRequired(emailError, email)
+  else if (email.validity.valueMissing) fieldRequired(emailError, email)
   
-  if (country.validity.valueMissing) fieldRequired(countryError, country)
+  else if (country.validity.valueMissing) fieldRequired(countryError, country)
 
-  if (zipcode.validity.valueMissing) fieldRequired(zipcodeError, zipcode)
+  else if (zipcode.validity.valueMissing) fieldRequired(zipcodeError, zipcode)
+
+  else if (password.validity.valueMissing) fieldRequired(passwordError, password)
+
+  else if (confirmPassword.validity.valueMissing) fieldRequired(confirmPasswordError, confirmPassword)
+
+  else if (password.value !== confirmPassword.value) {
+    msg.classList.add("error")
+    msg.textContent = "Passwords mismatch"
+  } 
+  
+  else {
+    msg.classList.add("success")
+    msg.textContent = "Sent Successfully"
+  }
 })
 
 //name validation
@@ -74,4 +95,19 @@ zipcode.addEventListener("input", (ev) => {
   if (zipcode.validity.valueMissing) fieldRequired(zipcodeError, zipcode)
   else if (!zipcode.validity.valid) invalidInput(zipcodeError, zipcode)
   else validInput(zipcodeError, zipcode)
+});
+
+//password validation
+password.addEventListener("input", (ev) => {
+  const passwordError = document.querySelector("#password-error")  
+  if (password.validity.valueMissing) fieldRequired(passwordError, password)
+  else if (!password.validity.valid) invalidInput(passwordError, password)
+  else validInput(passwordError, password)
+});
+
+confirmPassword.addEventListener("input", (ev) => {
+  const confirmPasswordError = document.querySelector("#confirmpassword-error")  
+  if (confirmPassword.validity.valueMissing) fieldRequired(confirmPasswordError, confirmPassword)
+  else if (!confirmPassword.validity.valid) invalidInput(confirmPasswordError, confirmPassword)
+  else validInput(confirmPasswordError, confirmPassword)
 });
